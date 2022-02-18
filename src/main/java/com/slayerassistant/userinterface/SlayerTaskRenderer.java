8b81d8383263fa26649b7e53c98f342f43be15db
LSO107 <34274877+LSO107@users.noreply.github.com>
2022-02-18 23:47:56 +0000

    add styling changes to select list

--- src/main/java/com/slayerassistant/userinterface/SlayerTaskRenderer.java
+++ src/main/java/com/slayerassistant/userinterface/SlayerTaskRenderer.java
@@ -3,6 +3,7 @@ package com.slayerassistant.userinterface;
 import com.slayerassistant.domain.SlayerTask;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class SlayerTaskRenderer extends JLabel implements ListCellRenderer<SlayerTask>
@@ -15,6 +16,13 @@ public class SlayerTaskRenderer extends JLabel implements ListCellRenderer<Slaye
             boolean isSelected,
             boolean cellHasFocus)
     {
+        setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setOpaque(isSelected);
+        setBackground(isSelected ? Color.DARK_GRAY : Color.GRAY);
+        setForeground(isSelected ? Color.WHITE : Color.LIGHT_GRAY);
+        setCursor(isSelected ? new Cursor(Cursor.HAND_CURSOR) : new Cursor(Cursor.DEFAULT_CURSOR));
+
         setText(value.monster);
         setVerticalAlignment(CENTER);
 
