01c1f8f719d69da126a8fe359259f861265ef59b
Lee <34274877+LeeOkeefe@users.noreply.github.com>
2024-07-27 02:17:29 +0100

    Update main panel to manage search and select panel

--- src/main/java/com/slayerassistant/rebuild/presentation/panels/MainPanel.java
+++ src/main/java/com/slayerassistant/rebuild/presentation/panels/MainPanel.java
@@ -1,32 +1,28 @@
 package com.slayerassistant.rebuild.presentation.panels;
 
 import com.slayerassistant.rebuild.domain.Task;
-import com.slayerassistant.rebuild.presentation.components.SearchBar;
-import com.slayerassistant.rebuild.presentation.components.SelectList;
 import com.slayerassistant.rebuild.services.TaskService;
-import com.slayerassistant.userinterface.SlayerTaskRenderer;
 import net.runelite.client.ui.PluginPanel;
 
 import javax.inject.Inject;
+import javax.swing.*;
 
 public class MainPanel extends PluginPanel
 {
     private final TaskService taskService;
-    private final SearchBar searchBar;
-    private final SelectList<Task> selectList;
+    
+    private final TaskSearchPanel taskSearchPanel = new TaskSearchPanel(this::onSearchBarChanged, this::onTaskSelected);
+    private final TaskSelectedPanel taskSelectedPanel = new TaskSelectedPanel();
     
     @Inject
     public MainPanel(TaskService taskService)
     {
         this.taskService = taskService;
         
-        searchBar = new SearchBar(this::onSearchBarChanged);
-        selectList = new SelectList<>(new SlayerTaskRenderer(), this::onTaskSelected);
+        add(taskSearchPanel);
+        add(taskSelectedPanel);
         
-        add(searchBar.getComponent());
-        add(selectList.getComponent());
-        
-        selectList.update(taskService.getAll());
+        taskSearchPanel.updateTaskList(taskService.getAll());
     }
     
     private void onSearchBarChanged(String searchTerm)
@@ -35,11 +31,14 @@ public class MainPanel extends PluginPanel
             ? taskService.getAll()
             : taskService.searchPartialName(searchTerm.trim());
         
-        selectList.update(matchedTasks);
+        taskSearchPanel.updateTaskList(matchedTasks);
     }
     
     private void onTaskSelected(Task task)
     {
-        System.out.println(task.name);
+        // TODO: Hide TaskSearch on task selected
+        // TODO: Update tab content to show selected task data
+        //
+        taskSelectedPanel.updateHeader(task.name, new ImageIcon(task.image));
     }
 }
