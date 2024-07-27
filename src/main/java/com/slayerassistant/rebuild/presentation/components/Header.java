e9684946d572a8dcf112a7a7a0b6a3d01843fe1c
Lee <34274877+LeeOkeefe@users.noreply.github.com>
2024-07-27 01:59:36 +0100

    Refactor header to extend JLabel

--- src/main/java/com/slayerassistant/rebuild/presentation/components/Header.java
+++ src/main/java/com/slayerassistant/rebuild/presentation/components/Header.java
@@ -3,30 +3,23 @@ package com.slayerassistant.rebuild.presentation.components;
 import javax.swing.*;
 import java.awt.*;
 
-public class Header extends JPanel
+public class Header extends JLabel
 {
-    private final JLabel label = new JLabel();
-    
     public Header()
     {
         Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 15);
         
-        label.setFont(font);
-        label.setForeground(Color.orange);
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalTextPosition(SwingConstants.TOP);
-        label.setHorizontalTextPosition(SwingConstants.CENTER);
-        label.setIconTextGap(10);
-    }
-    
-    public Component getComponent()
-    {
-        return label;
+        setFont(font);
+        setForeground(Color.orange);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalTextPosition(SwingConstants.TOP);
+        setHorizontalTextPosition(SwingConstants.CENTER);
+        setIconTextGap(10);
     }
     
     public void update(String title, ImageIcon icon)
     {
-        label.setText(title);
-        label.setIcon(icon);
+        setText(title);
+        setIcon(icon);
     }
 }
