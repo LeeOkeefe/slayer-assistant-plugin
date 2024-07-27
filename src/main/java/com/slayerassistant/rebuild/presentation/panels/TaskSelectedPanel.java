cf775df88ea9cf3994bf3dd7d4c354fa9dab1484
Lee <34274877+LeeOkeefe@users.noreply.github.com>
2024-07-28 00:34:15 +0100

    Update task selected panel to use tasks tab

--- src/main/java/com/slayerassistant/rebuild/presentation/panels/TaskSelectedPanel.java
+++ src/main/java/com/slayerassistant/rebuild/presentation/panels/TaskSelectedPanel.java
@@ -1,8 +1,8 @@
 package com.slayerassistant.rebuild.presentation.panels;
 
-import com.slayerassistant.rebuild.domain.Icon;
+import com.slayerassistant.rebuild.domain.Task;
 import com.slayerassistant.rebuild.presentation.components.Header;
-import net.runelite.client.ui.laf.RuneLiteTabbedPaneUI;
+import com.slayerassistant.rebuild.presentation.components.TaskTabs;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,55 +11,37 @@ import java.awt.event.ActionListener;
 public class TaskSelectedPanel extends JPanel 
 {
     private final Header header = new Header();
-    private final JTabbedPane tabbedPane = new JTabbedPane();
+    private final TaskTabs taskTabs = new TaskTabs();
     private final JButton closeButton = new JButton("Close");
     
     private final ActionListener onClickListener;
 
     public TaskSelectedPanel(Runnable onClose)
     {
-        setupTabbedPane();
         this.onClickListener = e -> onClose.run();
         closeButton.addActionListener(this.onClickListener);
         
         setLayout(new BorderLayout());
         
         add(header, BorderLayout.NORTH);
-        add(tabbedPane, BorderLayout.CENTER);
+        add(taskTabs, BorderLayout.CENTER);
         add(closeButton, BorderLayout.SOUTH);
     }
     
     public void shutDown()
     {
+        taskTabs.shutDown();
         closeButton.removeActionListener(onClickListener);
     }
     
-    public void updateHeader(String title, ImageIcon image)
+    public void update(Task task)
     {
-        header.update(title, image);
-    }
-
-    private void setupTabbedPane()
-    {
-        tabbedPane.setUI(new RuneLiteTabbedPaneUI()
-        {
-            @Override
-            protected int calculateTabWidth(int tabPlacement, int tabIndex, FontMetrics metrics)
-            {
-                return tabbedPane.getWidth() / tabbedPane.getTabCount();
-            }
-        });
-
-        addTab(Icon.COMPASS.getIcon(), new JTextArea(), "Locations");
-        addTab(Icon.INVENTORY.getIcon(), new JTextArea(), "Items required");
-        addTab(Icon.COMBAT.getIcon(), new JTextArea(), "Combat");
-        addTab(Icon.SLAYER_SKILL.getIcon(), new JTextArea(), "Slayer masters");
-        addTab(Icon.WIKI.getIcon(), new JTextArea(), "Wiki");
-    }
-
-    private void addTab(ImageIcon icon, Component component, String tip) 
-    {
-        tabbedPane.addTab(null, icon, component, tip);
+        header.update(task.name, new ImageIcon(task.image));
+        taskTabs.updateLocations(task.locations);
+        taskTabs.updateItemsRequired(task.itemsRequired);
+        taskTabs.updateCombat(task.attackStyles, task.attributes);
+        taskTabs.updateMasters(task.masters);
+        taskTabs.updateWikiLinks(task.wikiLinks);
     }
 }
 
