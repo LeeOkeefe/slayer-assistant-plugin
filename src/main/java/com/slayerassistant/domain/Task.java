f1b1224e0372ec0ec3fa32ff7e1ce9dd44434653
Lee <34274877+LeeOkeefe@users.noreply.github.com>
2024-08-06 08:21:42 +0100

    Introduce module for task service deps (#42)
    * Add task service module
    
    * Use constructor injection in TaskServiceImpl
    
    * Ignore properties from deserialization
    
    * Update build.gradle

--- src/main/java/com/slayerassistant/domain/Task.java
+++ src/main/java/com/slayerassistant/domain/Task.java
@@ -6,36 +6,36 @@ public class Task
 {
     public String name;
     public int levelRequired;
-    public BufferedImage image;
     public String[] itemsRequired;
     public String[] locations;
     public String[] attributes;
     public String[] attackStyles;
     public String[] variants;
     public String[] masters;
-    public WikiLink[] wikiLinks;
-
+    public transient BufferedImage image;
+    public transient WikiLink[] wikiLinks;
+    
     public Task(
             String name,
             int levelRequired,
-            BufferedImage image,
             String[] itemsRequired,
             String[] locations,
             String[] attributes,
             String[] attackStyles,
             String[] variants,
             String[] masters,
+            BufferedImage image,
             WikiLink[] wikiLinks) 
     {
         this.name = name;
         this.levelRequired = levelRequired;
-        this.image = image;
         this.itemsRequired = itemsRequired;
         this.locations = locations;
         this.attributes = attributes;
         this.attackStyles = attackStyles;
         this.variants = variants;
         this.masters = masters;
+        this.image = image;
         this.wikiLinks = wikiLinks;
     }
 }
